1830c74e74e34da3d4f9fd126751ef139b024a8b
hezhi <15119628580@163.com>
2019-05-23 22:02:44 +0800

    记录发放

--- 03-mistake.js
+++ 03-mistake.js
@@ -33,3 +33,7 @@ app.use('/',function(err,req,res,next){
 app.listen(3000,function(){
   console.log('请访问http://127.0.0.1:3000');
 })
+
+app.listen(3000,function(){
+  console.logahofhaoihegoiaheiherhwoegaowgangkawnfkandkfhei 
+});
